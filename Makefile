6a7f37e6045981c27db6f51a7696d9f8f9573dde
Jintao Koong <jintaokoong99@gmail.com>
2021-08-14 18:17:12 +0800

    fix: fix month day position
    - fix log file name month day position
    - update makefile to include target arm arch

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 build:
 	go build -o bin/tcl tcl.go
+build-arm:
+	GOOS=linux GOARCH=arm go build bin/tcl-arm tcl.go
 run:
 	go run tcl.go
--- utils/file.go
+++ utils/file.go
@@ -8,6 +8,6 @@ import (
 
 func GetFile(prefix string) (*os.File, error) {
 	ct := time.Now()
-	fn := fmt.Sprintf("%s_%s.log", prefix, ct.Format("20060201"))
+	fn := fmt.Sprintf("%s_%s.log", prefix, ct.Format("20060102"))
 	return os.OpenFile(fn, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 }
